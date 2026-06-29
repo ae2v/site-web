3c9e0e00909339aa394f8ab0fc320f4634464f33
Loan <loan.jean@ens.uvsq.fr>
2026-06-29 13:11:29 +0200

    Optimisations sur la page d'accueil
    * Utilisation des balises meta pour le titre (à venir pour le SEO)
    * Ajout des événements uniquement si y'en a
    * Redesign général

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 import Navbar from "~/components/layout/Navbar.vue";
 import Button from "~/components/Button.vue";
+
+import EventCard from "~/components/cards/EventCard.vue";
+
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/vue/20/solid";
 
 import { ref } from "vue";
 import { useInView } from "motion-v";
 
+const { events } = useEvents();
+
 useHead({
+	title: "BDE de Vélizy",
+	meta: [
+		{
+			name: "title",
+			content: "BDE de Vélizy",
+		},
+		{
+			property: "og:title",
+			content: "BDE de Vélizy",
+		}
+	],
 	bodyAttrs: {
 		class: "bg-[url('/hero_bde.jpg')] bg-cover bg-center bg-no-repeat bg-fixed",
 	},
@@ -21,9 +37,11 @@ const isHeaderVisible = useInView(headerRef, {
 	<Navbar :isTransparent="isHeaderVisible" :isFixed="true" />
 	<header
 		ref="headerRef"
-		class="flex flex-col items-center justify-center gap-8 bg-background h-screen"
+		class="flex flex-col items-center justify-center gap-8 bg-black/30 text-white h-screen"
 	>
-		<h1 class="text-7xl font-bold font-display text-center">BDE de Vélizy</h1>
+		<h1 class="text-7xl font-bold font-display text-center">
+			BDE de Vélizy
+		</h1>
 		<p class="text-xl text-center max-w-2xl">
 			Découvrez nos événements, nos projets et notre équipe.
 			Rejoignez-nous pour vivre une expérience étudiante inoubliable !
@@ -36,20 +54,20 @@ const isHeaderVisible = useInView(headerRef, {
 			:icon="ChatBubbleOvalLeftEllipsisIcon"
 		/>
 	</header>
-	<main class="bg-black/30 text-white min-h-screen">
-		<section class="py-16 px-8">
+	<main class="bg-background min-h-screen">
+		<section class="py-16 px-8" v-if="events.length > 0">
 			<h2 class="text-4xl font-bold font-title text-center mb-8">
 				Nos événements à venir
 			</h2>
 			<div
 				class="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
-			></div>
-		</section>
-		<section class="py-16 px-8">
-			<h2 class="text-4xl font-bold font-title text-center">
-				Nos goodies
-			</h2>
-			<!-- Liste des goodies -->
+			>
+				<EventCard
+					v-for="event in events"
+					:key="event.id"
+					:event="event"
+				/>
+			</div>
 		</section>
 		<section class="py-16 px-8 space-y-8">
 			<h2 class="text-4xl font-bold font-title text-center">
